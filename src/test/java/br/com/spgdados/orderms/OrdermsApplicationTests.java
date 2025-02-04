ae5a9ee35ba34757ded31b88282ee33270920296
Sidinei P. Gonchoroski <sidinei.pereira@gmail.com>
2025-02-04 20:53:49 -0300

    feat: initial commit

---
+++ src/test/java/br/com/spgdados/orderms/OrdermsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spgdados.orderms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
